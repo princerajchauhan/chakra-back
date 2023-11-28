6a0d61c1fec154d6e5deebb948635ddb00aad813
Prince <princeraj.chauhan@outlook.com>
2023-11-28 14:16:08 +0530

    new link

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const startConnection = async () => {
         const io = socket(app, {
             pingTimeout: 60000,
             cors: {
-                origin: "https://prince-chat.vercel.app/"
+                origin: "*"
             }
         })
 
